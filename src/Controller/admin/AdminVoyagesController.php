4f883aad6b05964d37d5a5073fa1873e1c42147c
Patrick <186724585+patrickbrouhard@users.noreply.github.com>
2025-11-11 20:12:15 +0100

    Phase 11 : ORM (suppression d'un enregistrement).

---
+++ src/Controller/admin/AdminVoyagesController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Controller\admin;
+
+use App\Repository\VisiteRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * Description of AdminVoyagesController
+ *
+ * @author Patrick
+ */
+class AdminVoyagesController extends AbstractController {
+    
+    /**
+     * 
+     * @var VisiteRepository
+     */
+    private $repository;
+       
+    /**
+     * 
+     * @param VisiteRepository $repository
+     */
+    public function __construct(VisiteRepository $repository) {
+        $this->repository = $repository;
+    }
+    
+    #[Route('/admin', name: 'admin.voyages')]
+    public function index() : Response {
+        $visites = $this->repository->findAllOrderBy('datecreation', 'DESC');
+        return $this->render("admin/admin.voyages.html.twig", [
+            'visites' => $visites
+        ]);
+    }
+    
+    #[Route('/admin/suppr/{id}', name: 'admin.voyage.suppr')]
+    public function suppr(int $id) : Response
+    {
+        $visite = $this->repository->find($id);
+        $this->repository->remove($visite);
+        return $this->redirectToRoute('admin.voyages');
+    }
+}
